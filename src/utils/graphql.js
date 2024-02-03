c52d1177940ef2913d5a110ed55acb9c36679eb0
Julio Ribera <julio@magiaycardistry.com>
2024-02-03 11:38:41 +0100

    Productos estáticos

--- src/utils/graphql.js
+++ src/utils/graphql.js
@@ -1,4 +1,4 @@
-// src/api/graphql.js
+// REALIZAR CONSULTAS A LA API DE SALEOR
 async function fetchGraphQL(query, variables = {}) {
   const endpoint = "https://store-cs29ogoa.eu.saleor.cloud/graphql/";
   const response = await fetch(endpoint, {
@@ -16,6 +16,43 @@ async function fetchGraphQL(query, variables = {}) {
   return data;
 }
 
+// LISTA DE PRODUCTOS
+export async function getProducts(first = 30, channel = "default-channel") {
+  const query = `{
+    products(first: ${first}, channel: "${channel}") {
+      edges {
+        node {
+          id
+          name
+          isAvailableForPurchase
+          pricing {
+            priceRange {
+              start {
+                net {
+                  amount
+                }
+              }
+            }
+          }
+          thumbnail(format: WEBP, size: 300) {
+            url
+          }
+          images {
+            url(format: WEBP)
+          }
+          description
+          slug
+        }
+      }
+      totalCount
+    }
+  }`;
+
+  // No necesitas variables aquí, pero si la consulta las tuviera, podrías pasarlas como segundo argumento
+  return await fetchGraphQL(query);
+}
+
+// DETALLE DE UN PRODUCTO
 export async function getProductBySlug(slug) {
   // Nota cómo se utiliza $slug para definir la variable dentro de la consulta GraphQL
   const query = `query ProductBySlug($slug: String!) {
